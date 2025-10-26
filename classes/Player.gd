c70094a181d3474855fc5eeb059636024deee3cc
foticc <foticc@163.com>
2025-10-27 00:12:12 +0800

    1026

--- classes/Player.gd
+++ classes/Player.gd
@@ -3,26 +3,63 @@ class_name Player
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-var state_machine: StateMachine
+enum State {
+	IDLE,
+	WALK
+}
+
+const SPEED = 300.0
 
 var default_gravity := ProjectSettings.get_setting("physics/2d/default_gravity") as float
 
 func _ready() -> void:
-	self.state_machine = StateMachine.new(self)
-	self.state_machine.add_state("walk",IdleState.new())
-	self.state_machine.change_state("walk")
 	print(default_gravity)
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action("right"):
-		state_machine.change_state("walk")
+	pass
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
+	pass
+
+func transition_state(from:State,to:State) -> void:
+	print("[%s]from [%s]--->[%s]"%
+		[
+			Engine.get_physics_frames(),
+			State.keys()[from] if from != -1 else "START",
+			State.keys()[to]
+		]
+	)
+	match to:
+		State.IDLE:
+			animated_sprite.play("idle")
+		State.WALK:
+			animated_sprite.play("run")
+
+func get_next_state(current:State)-> State:
+	var direction := Input.get_axis("left", "right")
+	var is_still := is_zero_approx(direction) and is_zero_approx(velocity.x)
+	match current:
+		State.IDLE:
+			if not is_still:
+				return State.WALK
+		State.WALK:
+			if is_still:
+				return State.IDLE
+	return current
+
+func tick_physics(state:State,delta:float)->void:
+	var direction := Input.get_axis("left", "right")
+
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	self.state_machine.update(delta)
-
-
-func play_anim(anim:String)->void:
-	self.animated_sprite.play(anim)
+		velocity.y += default_gravity * delta
+	match state:
+		State.IDLE:
+			pass
+		State.WALK:
+			if direction:
+				velocity.x = move_toward(velocity.x,direction * SPEED,SPEED/0.2) 
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
+	if not is_zero_approx(direction):
+		animated_sprite.scale.x = -1 if direction<0 else 1
+	move_and_slide()
--- classes/StateMachine.gd
+++ classes/StateMachine.gd
@@ -1,28 +1,24 @@
-extends RefCounted
-class_name StateMachine
+class_name StateMachine extends Node
+
+var current_state: int = -1:
+	set(state):
+		owner.transition_state(current_state,state)
+		current_state = state
+		state_time = 0
+# 注意类型！！！！！！！！！ 要么加类型float 要么设置初值为0.0 来表达float
+# 否则的话 下方的 state_time+=delta 会不正确，被强转为0 导致每一帧都加不到1，永远为0
+var state_time:float = 0
+
+func _ready() -> void:
+	await owner.ready
+	current_state = 0
 
 
-var owner: Node
-var current_state: State
-var states := {}
-
-func _init(_owner: Node) -> void:
-	self.owner = _owner
-
-func add_state(name: String, state: State) -> void:
-	states[name] = state
-
-
-func change_state(name: String) -> void:
-	if not states.has(name):
-		push_error("State %s not found!" % name)
-		return
-	if current_state:
-		current_state.exit()
-	current_state = states[name]
-	print(current_state.get_real_class_name())
-	current_state.enter(owner)
-
-func update(delta: float) -> void:
-	if current_state:
-		current_state.update(delta)
+func _physics_process(delta: float) -> void:
+	while true:
+		var next = owner.get_next_state(current_state) as int
+		if current_state == next:
+			break
+		current_state = next
+	owner.tick_physics(current_state,delta)
+	state_time+=delta
